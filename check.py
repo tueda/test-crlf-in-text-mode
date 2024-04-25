3eae91f879c49d753ace03935fb464cd61e13f96
Takahiro Ueda <takahiro.ueda@yahoo.com>
2024-04-25 19:49:41 +0900

    feat(check.py): improve handling of CR/LF

--- check.py
+++ check.py
@@ -1,8 +1,10 @@
 import sys
+import re
 
 with open(sys.argv[1], "rb") as f:
     s = f.read().decode("utf-8")
 s = s.replace("\r", "<CR>")
 s = s.replace("\n", "<LF>")
+s = re.sub("(<CR>|<LF>)+", "\\g<0>\n", s)
 s = s.replace("<BR>", "\n")
 print(s)
--- main.c
+++ main.c
@@ -4,9 +4,7 @@ int main()
 {
     FILE *f = fopen("out.txt", "wt");
     fprintf(f, "CR: \r");
-    fprintf(f, "<BR>");
     fprintf(f, "LF: \n");
-    fprintf(f, "<BR>");
     fprintf(f, "CR+LF: \r\n");
     fclose(f);
     return 0;
